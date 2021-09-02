9b6a761ec24688b11f338f61490915504802f2ed
jakubk <39801439+jakubk69@users.noreply.github.com>
2021-09-02 08:55:54 +0200

    Add files via upload

--- jakubKielbasinski4Tb/index.php
+++ jakubKielbasinski4Tb/index.php
@@ -1,6 +1,50 @@
-<?php
-$x = "tekst";
-$y = "tekst 2";
-echo "<p style='text-align: center; text-color: red' >$x</p>";
-print "<p style='text-align: right'>$y</p>";
-?>
+<!DOCTYPE html>
+<html>
+      <head>
+      </head>
+      <body>
+            <?php
+                  $number = 2;
+                  
+                  if([$number] > 0)
+                  {
+                        echo "<p>$number jest liczba dodatnia</p>";
+                  }
+                  elseif([$number] < 0)
+                  {
+                        echo "<p>$number jest liczba ujemna</p>";
+                  }
+                  elseif([$number] == '0')
+                  {
+                        echo "<p>Zero</p>";
+                  }
+                  else
+                  {
+                        echo "<p>Podaj liczbę</p>";
+                  }
+                  
+                  $i = 0;
+                  while ($i <= 20) {
+                        $i++;
+                        if ($i % 2 == 1) continue;
+                        echo $i . "<br>";
+                        if ($i >= 10) break;                        
+                  }
+
+                  switch ($_GET['kolor']) 
+                  {
+                        case "czerwony":
+                        echo "Twój ulubiony kolor to czerwony!";
+                        break;
+                        case "niebieski":
+                        echo "Twój ulubiony kolor to niebieski!";
+                        break;
+                        case "zielony":
+                        echo "Twój ulubiony kolor to zielony!";
+                        break;
+                        default:
+                        echo "Twój ulubiony kolor to nie czerwony, niebieski ani zielony!";
+                  }
+            ?>
+      </body>
+</html>
